1deb7e268d1c21c80d18f30e6ec5fe36e2d00495
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-14 17:59:01 +0800

    feat: remove create & create another button

--- app/Filament/Resources/PostResource/Pages/CreatePost.php
+++ app/Filament/Resources/PostResource/Pages/CreatePost.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Pages\Actions\ButtonAction;
 
 class CreatePost extends CreateRecord
 {
@@ -19,5 +20,29 @@ class CreatePost extends CreateRecord
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
+    }
+
+    protected function getFormActions(): array
+    {
+        return [
+             ButtonAction::make('submit')
+             ->label('Create')
+             ->action('submit')
+             ->color('primary')
+             ->requiresConfirmation(false),  
+
+         // Cancel button
+         ButtonAction::make('cancel')
+             ->label('Cancel')
+             ->url($this->getRedirectUrl())
+             ->color('gray'),
+        ];
+    }
+
+    public function submit()
+    {
+        $this->create();
+
+        return redirect()->to($this->getRedirectUrl());
     }
 }
