90d855aacc39611eb67c24d2532b80202c29dece
Archie Jefferson Em <archie.em99@gmail.com>
2024-11-14 18:06:33 +0800

    feat: update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        \App\Models\User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('password')
         ]);
+
+        \App\Models\User::factory(11)->create();
+        \App\Models\Post::factory(11)->create();
     }
 }
